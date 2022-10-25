bbe0f3dcf3d8aff02c606e471b0bc8f61f5e4ba4
Erik Simon <erik.simon.me@gmail.com>
2022-10-26 01:00:07 +0200

    b

--- install.sh
+++ install.sh
@@ -6,5 +6,4 @@ cargo build --profile release
 
 sudo chmod +x target/release/vlc-autoskip
 cp target/release/vlc-autoskip ~/.local/bin/vlc-autoskip
-cp settings.json ~/.config/vlc-autoskip/settings.json
 cp VLCAutoskip.desktop ~/.local/share/applications/VLCAutoskip.desktop
