d39381244f309542ae761fcf769b33555242ccc6
Erik Simon <erik.simon.me@gmail.com>
2022-10-26 00:19:34 +0200

    124567

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
-#!/usr/bin bash
+#!/usr/bin/env bash
+mkdir -p ~/.config/vlc-autoskip
+mkdir -p ~/.local/bin
 
 cargo build --profile release
+
+sudo chmod +x target/release/vlc-autoskip
 cp target/release/vlc-autoskip ~/.local/bin/vlc-autoskip
-chmod +x ~/.local/bin/vlc-autoskip
+cp settings.json ~/.config/vlc-autoskip/settings.json
 
-mkdir ~/.config/vlc-autoskip
-cp ./settings.json ~/.config/vlc-autoskip/settings.json
-
+rm ~/.local/share/applications/VLCAutoskip.desktop
 cat <<EOT >> ~/.local/share/applications/VLCAutoskip.desktop
 [Desktop Entry]
 Name=VLCAutoskip
 Exec=vlc-autoskip
-Icon=steam_icon_945360
+Icon=vlc
 Terminal=true
 Type=Application
-Categories=Other;
 EOT
